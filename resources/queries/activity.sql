a21179464dc3079b548f774ef4e4efe5cf49c1c4
emre <iemrek@gmail.com>
2018-06-24 19:17:26 +0000

    Add endpoint for updating Activity

--- resources/queries/activity.sql
+++ resources/queries/activity.sql
@@ -10,3 +10,10 @@ where stripe_id=:stripe_id
 -- name: insert!
 insert into activities (note, start_date, length_in_minutes, stripe_id, user_id, created_at)
  values (:note, :start_date, :length_in_minutes, :stripe_id, :user_id, :created_at);
+
+-- name: update!
+update activities set
+note = coalesce(:note, note),
+start_date = coalesce(:start_date, start_date),
+length_in_minutes = coalesce(:length_in_minutes, length_in_minutes)
+where id=:id;
--- src/life_stripes_api/common/date_utils.clj
+++ src/life_stripes_api/common/date_utils.clj
@@ -3,4 +3,6 @@
            (java.sql Timestamp)))
 
 (defn parse-iso8601-as-timestamp [date-str]
-  (Timestamp. (.getTime (.parse (SimpleDateFormat. "yyyy-MM-dd'T'hh:MM:ss") date-str))))
+  (if date-str
+    (Timestamp. (.getTime (.parse (SimpleDateFormat. "yyyy-MM-dd'T'hh:MM:ss") date-str)))
+    nil))
